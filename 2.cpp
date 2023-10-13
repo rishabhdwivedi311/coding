894417653c606d02e778d932827a28d365e19c5c
rishabhdwivedi311 <rishabhdwivedi311@gmail.com>
2023-10-13 15:22:32 +0530

    first commit to coding

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using  namespace std;
+
+int main()
+{
+int a ,b;
+cout<<"enter the first number : " ;
+cin>>a;
+cout<<"enter the second number : " ; 
+cin>>b;
+cout<<"th sum of anumber is:  " <<a+b<<endl;
+
+return 0;
+
+}
